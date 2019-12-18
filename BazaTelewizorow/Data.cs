72cc93230d970cd9a1c8cd43ce40a60bced93edc
Dawid Halada <dawid.hal@gmail.com>
2019-12-18 16:54:05 +0100

    dawd

---
+++ BazaTelewizorow/Data.cs
@@ -0,0 +1,245 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace BazaTelewizorow
+{
+    public class Losowator
+    {
+        static Random random = new Random(Guid.NewGuid().GetHashCode()); //losowator o takim seedzie że łohohoho
+        public static string RandomBrand() // losowanie marki z puli jaka jest w pliku marka.txt
+        {
+            var randBrand = random.Next(0, 26);
+            using (StreamReader inputFile = new StreamReader("marka.txt"))
+            {
+                for (int i = 1; i < randBrand; i++)
+                {
+                    inputFile.ReadLine();
+                }
+
+                return inputFile.ReadLine();
+            }
+        }
+        public static int RandomRozdzielczosc() //losowa rozdzielczosc z puli
+        {
+            int[] rozdzielczosc = { 1280, 2560, 1920, 1680, 2560, 3440, 4096, 480, 1440 };
+            var randBrand = random.Next(0, rozdzielczosc.Length);
+            return rozdzielczosc[randBrand];
+        }
+        public static bool RandomSprzedaz() //losowy boolean true/false
+        {
+            if (random.Next(0, 2) == 1)
+            {
+                return true;
+            }
+            else { return false; }
+        }
+        public static string RandomSeria(int length) //losowa seria
+        {
+
+            const string chars = "0123456789";
+            return new string(Enumerable.Repeat(chars, length)
+              .Select(s => s[random.Next(s.Length)]).ToArray());
+        }
+        public static string RandomStringOpis() //losowy opis łączy kilka wyrazów z pul
+        {
+            string zdanie = "Telewizor jest ";
+            string[] wyraz = { "super", "czarny", "biały", "elegancki", "ekstra", "srebrny" };
+            zdanie += wyraz[random.Next(0, wyraz.Length)];
+            return zdanie;
+        }
+        public static string RandomCal() //losowa ilość cali
+        {
+
+            const string chars = "0123456789";
+            return (new string(Enumerable.Repeat(chars, 2)
+              .Select(s => s[random.Next(s.Length)]).ToArray())
+                + "," + new string(Enumerable.Repeat(chars, 2)
+              .Select(s => s[random.Next(s.Length)]).ToArray()));
+        }
+        public static string RandomCena()//losowa cena
+        {
+
+            const string chars = "0123456789";
+            return (new string(Enumerable.Repeat(chars, 4)
+              .Select(s => s[random.Next(s.Length)]).ToArray())
+                + "," + new string(Enumerable.Repeat(chars, 2)
+              .Select(s => s[random.Next(s.Length)]).ToArray()));
+        }
+    }
+    public class DataClass //jednostka danych
+    {
+        //wszystkie wartości jakie posiada DataClass z możliwością odczytu i zapisu
+        public int IDelem { get; set; }
+        public string brand { get; set; }
+        public string kodproduktu { get; set; }
+        public int rozdzielczosc { get; set; }
+        public int nrSerii { get; set; }
+        public bool sprzedaz { get; set; }
+        public double cal { get; set; }
+        public double cena { get; set; }
+        public string opis { get; set; }
+
+        public DataClass(string brak = "brak") //używać ostrożnie
+        {
+            IDelem = 0;
+
+            kodproduktu = brak;
+            brand = brak;
+            rozdzielczosc = 0;
+            nrSerii = 0;
+            sprzedaz = false;
+            opis = brak;
+            cal = 0;
+            cena = 0;
+        }
+        public DataClass(int id) //inicjalizacja i losowanie
+        {
+            IDelem = id;
+            Random random = new Random(Guid.NewGuid().GetHashCode());
+            //kodproduktu = Losowator.RandomString(10);
+            //brand = Losowator.RandomBrand();
+            //rozdzielczosc = Losowator.RandomRozdzielczosc();
+            //sprzedaz = Losowator.RandomSprzedaz();
+            //opis = Losowator.RandomStringOpis();
+            //cal = double.Parse(Losowator.RandomCal());
+            //cena = double.Parse(Losowator.RandomCena());
+            nrSerii = int.Parse(Losowator.RandomSeria(5));
+        }
+        public DataClass(
+        int id = 0,
+        string br = "",
+        string kod = "",
+        int rozdz = 0,
+        int nrserii = 0,
+        bool sprze = false,
+        double cal1 = 0,
+        double cena1 = 0,
+        string opis1 = "") //używany przy dodawaniu elementów, jeżeli jakiś nie zostanie wypełniony, przyjmie wartość startową
+        {
+            this.IDelem = id;
+            this.brand = br;
+            this.kodproduktu = kod;
+            this.rozdzielczosc = rozdz < 0 ? 0 : rozdz;
+            this.nrSerii = nrserii < 0 ? 0 : nrserii;
+            this.sprzedaz = sprze;
+            this.cal = cal1 < 0 ? 0 : cal1;
+            this.cena = cena1 < 0 ? 0 : cena1;
+            this.opis = opis1;
+        }
+
+    }
+    public class Sortowanie
+    {
+        DataClass[] tablica;
+
+        public Sortowanie(List<DataClass> tablica)
+        {
+            this.tablica = tablica.ToArray();
+        }
+        //wyszukiwanie binarne po NRserii, więc sortowanie po nrserii
+        public void sortowanieNrSeriiBubbleSort() //sortowanie bąbelkowe
+        {
+            for (int p = 0; p < tablica.Length; p++)
+            {
+                for (int q = 1; q < tablica.Length; q++)
+                {
+                    if (tablica[q - 1].nrSerii > tablica[q].nrSerii)
+                    {
+                        Swap(q - 1, q);
+                    }
+                }
+            }
+
+        }
+
+        public void Swap(int first, int second)//zamiana miejscami w tablicy
+        {
+            var temporary = tablica[first];
+            tablica[first] = tablica[second];
+            tablica[second] = temporary;
+        }
+
+        public void sortowanieNrSeriiSelectionSort()//sortowanie przez selekcje
+        {
+
+            int smallest;
+            for (int i = 0; i < tablica.Length - 1; i++)
+            {
+                smallest = i;
+
+                for (int index = i + 1; index < tablica.Length; index++)
+                {
+                    if (tablica[index].nrSerii < tablica[smallest].nrSerii)
+                    {
+                        smallest = index;
+                    }
+                }
+                Swap(i, smallest);
+
+
+            }
+        }
+        public DataClass[] zwrot() //pobranie tablicy z klasy sortowej
+        {
+            return this.tablica;
+        }
+    }
+    public class Wyszukiwanie
+    {
+        static string brak = "brak"; //gdyby nie znalazło
+        DataClass[] tablica;
+        DataClass wynik = new DataClass(brak);
+        List<DataClass> wynikList = new List<DataClass>();
+
+        public Wyszukiwanie(List<DataClass> tablica)//wpisanie listy do tablicy
+        {
+            this.tablica = tablica.ToArray();
+        }
+        public List<DataClass> liniowe(int szukana)//wyszukiwanie liniowe
+        {
+            //int d = 0;
+            foreach (var t in tablica)
+            {
+                //d++;
+                if (t.nrSerii == szukana)
+                {
+                    wynikList.Add(t);
+                    //System.Console.WriteLine("moje d " + d);
+                    return wynikList;
+                }
+            }
+
+            return wynikList;
+        }
+        public DataClass binarne(int szukana) //wyszukiwanie binarne
+        {
+            //int d = 0;
+            int l, p;
+            l = 0;
+            p = tablica.Length - 1;
+            int sr;
+            while (l <= p)
+            {
+                //d++;
+                sr = (l + p) / 2;
+                //System.Console.WriteLine("moje d " + d); //sprawdzam tym fakt, ilości podziałów binarnych
+                if (tablica[sr].nrSerii == szukana)
+                {
+                    wynik = tablica[sr];
+                    return wynik;
+                }
+                if (tablica[sr].nrSerii > szukana)
+                    p = sr - 1;
+                else
+                    l = sr + 1;
+            }
+            return wynik;
+
+
+
+        }
+
+    }
+}
--- BazaTelewizorow/MainWindow.xaml.cs
+++ BazaTelewizorow/MainWindow.xaml.cs
@@ -1,28 +1,437 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
+using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace BazaTelewizorow
 {
-    /// <summary>
-    /// Logika interakcji dla klasy MainWindow.xaml
-    /// </summary>
+
+
     public partial class MainWindow : Window
     {
+        List<DataClass> dataClass = new List<DataClass>();
+        static public int i = 0;
+        string path = @"C:\Users\dawid\Desktop\Uczelnia\Zarządzanie jednostką informacyjną\BazaTelewizorow\BazaTelewizorow\plik.txt";
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Losuj_Click(object sender, RoutedEventArgs e) //losowanie 
+        {
+            if (Text.Text.Length < 1)
+                return;
+
+            int ilosc = 0;
+
+            try
+            {
+                ilosc = int.Parse(Text.Text);
+            }
+            catch
+            {
+                MessageBox.Show("nie litery");
+            }
+
+
+
+
+
+
+            while (ilosc > 0)
+            {
+                dataClass.Add(new DataClass(++i));
+                ilosc--;
+            }
+            Tabela.ItemsSource = null;
+            Tabela.ItemsSource = dataClass;
+            Tabela.DataContext = dataClass;
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e) //usuwanie
+        {
+            if (Text.Text.Length < 1)
+                return;
+            try //tylko indeks
+            {
+                int p = int.Parse(NrUsuwania.Text);
+            }
+            catch
+            {
+                MessageBox.Show("Nie litery");
+                return;
+            }
+            int numer = int.Parse(NrUsuwania.Text);
+            DataClass[] tabela = dataClass.ToArray();
+            List<DataClass> dataClassWynik = new List<DataClass>();
+
+            if (numer <= 0)
+            {
+                return;
+            }
+
+            for (int d = 0; d < tabela.Length; d++)
+            {
+                if (tabela[d].IDelem != numer)
+                {
+                    dataClassWynik.Add(dataClass[d]);
+                }
+            }
+            dataClass.Clear();
+            dataClass = dataClassWynik;
+
+            Tabela.ItemsSource = null;
+            Tabela.ItemsSource = dataClass;
+            Tabela.DataContext = dataClass;
+        }
+
+        private void dodawaniePrzycisk_Click(object sender, RoutedEventArgs e)
+        {
+
+            try
+            {
+                int d = int.Parse(RozDodawanie.Text);
+
+                if (d < 0)
+                {
+                    MessageBox.Show("Nie ujemne");
+                    return;
+                }
+
+            }
+            catch
+            {
+                MessageBox.Show("Nie litery");
+                return;
+            }
+            try
+            {
+                int a = int.Parse(NrSeriiDodawanie.Text);
+
+                if (a < 0)
+                {
+                    MessageBox.Show("Nie ujemne");
+                    return;
+                }
+
+            }
+            catch
+            {
+                MessageBox.Show("Nie litery");
+                return;
+            }
+            try
+            {
+                double w = double.Parse(CalDodawanie.Text);
+
+                if (w < 0)
+                {
+                    MessageBox.Show("Nie ujemne");
+                    return;
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Nie litery");
+                return;
+            }
+            try
+            {
+                double i = double.Parse(CenaDodawanie.Text);
+                if (i < 0)
+                {
+                    MessageBox.Show("Nie ujemne");
+                    return;
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Nie litery");
+                return;
+            }
+            string br = MarkaDodawanie.Text;
+            string kod = KodDodawanie.Text;
+            int rozdz = int.Parse(RozDodawanie.Text);
+            int nrserii = int.Parse(NrSeriiDodawanie.Text);
+            bool sprze = sprzedaz.IsChecked ?? true;
+            double cal1 = double.Parse(CalDodawanie.Text);
+            double cena1 = double.Parse(CenaDodawanie.Text);
+            string opis1 = OpisDodawanie.Text;
+
+            dataClass.Add(new DataClass(++i, br, kod, rozdz, nrserii, sprze, cal1, cena1, opis1));
+
+            Tabela.ItemsSource = null;
+            Tabela.ItemsSource = dataClass;
+            Tabela.DataContext = dataClass;
+        }
+
+        private void wczytaj_Click(object sender, RoutedEventArgs e)
+        {
+
+            using (StreamReader sr = File.OpenText(path))
+            {
+                while (!sr.EndOfStream)
+                {
+                    var line = sr.ReadLine();
+                    string[] lineWords = line.Split('`');
+                    dataClass.Add(new DataClass(int.Parse(lineWords[0]), lineWords[1], lineWords[2], int.Parse(lineWords[3]), int.Parse(lineWords[4]), bool.Parse(lineWords[5]), double.Parse(lineWords[6]), double.Parse(lineWords[7]), lineWords[8]));
+
+                }
+
+
+
+
+                sr.Close();
+                Tabela.ItemsSource = null;
+                Tabela.ItemsSource = dataClass;
+                Tabela.DataContext = dataClass;
+            }
+        }
+
+        private void zapisz_Click(object sender, RoutedEventArgs e)
+        {
+            using (StreamWriter sr = new StreamWriter(path))
+            {
+                foreach (var i in dataClass)
+                {
+                    sr.Write(i.IDelem);
+                    sr.Write("`");
+                    sr.Write(i.brand);
+                    sr.Write("`");
+                    sr.Write(i.kodproduktu);
+                    sr.Write("`");
+                    sr.Write(i.rozdzielczosc);
+                    sr.Write("`");
+                    sr.Write(i.nrSerii);
+                    sr.Write("`");
+                    sr.Write(i.sprzedaz);
+                    sr.Write("`");
+                    sr.Write(i.cal);
+                    sr.Write("`");
+                    sr.Write(i.cena);
+                    sr.Write("`");
+                    sr.Write(i.opis);
+                    sr.Write("\n");
+
+                }
+
+
+                sr.Close();
+
+            }
+        }
+
+        private void kasujwszystko_Click(object sender, RoutedEventArgs e)
+        {
+
+
+
+            for (int g = 0; g < dataClass.Count();)
+            {
+                dataClass.Remove(dataClass[g]);
+            }
+            Tabela.ItemsSource = null;
+            Tabela.ItemsSource = dataClass;
+            Tabela.DataContext = dataClass;
+
+            i = 0;
+        }
+
+        private void SzukaniePrzycisk_Click(object sender, RoutedEventArgs e)
+        {
+
+            if (KodSzukanie.Text == "" || RozSzukanie.Text == "" || CalSzukanie.Text == "" || CenaSzukanie.Text == "")
+                return;
+
+
+            string br = MarkaSzukanie.Text;
+            string kod = KodSzukanie.Text;
+            int rozdz = int.Parse(RozSzukanie.Text);
+            int nrserii = int.Parse(NrSeriiSzukanie.Text);
+            double cal1 = double.Parse(CalSzukanie.Text);
+            double cena1 = double.Parse(CenaSzukanie.Text);
+            string opis1 = OpisSzukanie.Text;
+
+
+            DataClass[] tabela = dataClass.ToArray();
+
+            List<DataClass> dataClassWynik = new List<DataClass>();
+
+            if (br.Length >= 1)
+            {
+                foreach (var t in tabela)
+                {
+                    if (t.brand == br.ToUpper())
+                    {
+                        dataClassWynik.Add(t);
+                    }
+                }
+            }
+            if (kod.Length >= 1)
+            {
+                foreach (var t in tabela)
+                {
+                    if (t.kodproduktu == kod.ToUpper())
+                    {
+                        dataClassWynik.Add(t);
+                    }
+                }
+            }
+            if (rozdz >= 1)
+            {
+                foreach (var t in tabela)
+                {
+                    if (t.rozdzielczosc == rozdz)
+                    {
+                        dataClassWynik.Add(t);
+                    }
+                }
+            }
+            if (nrserii >= 1)
+            {
+                foreach (var t in tabela)
+                {
+                    if (t.nrSerii == nrserii)
+                    {
+                        dataClassWynik.Add(t);
+                    }
+                }
+            }
+            if (cal1 >= 1)
+            {
+                foreach (var t in tabela)
+                {
+                    if (t.cal == cal1)
+                    {
+                        dataClassWynik.Add(t);
+                    }
+                }
+            }
+            if (cena1 >= 1)
+            {
+                foreach (var t in tabela)
+                {
+                    if (t.cena == cena1)
+                    {
+                        dataClassWynik.Add(t);
+                    }
+                }
+            }
+            if (opis1.Length >= 1)
+            {
+                foreach (var t in tabela)
+                {
+                    if (t.opis == opis1)
+                    {
+                        dataClassWynik.Add(t);
+                    }
+                }
+            }
+
+            Tabela.ItemsSource = null;
+            Tabela.ItemsSource = dataClassWynik;
+            Tabela.DataContext = dataClassWynik;
+        }
+
+        private void wrocSzukanie_Click(object sender, RoutedEventArgs e)
+        {
+            Tabela.ItemsSource = null;
+            Tabela.ItemsSource = dataClass;
+            Tabela.DataContext = dataClass;
+        }
+
+        private void coNtyPrzycisk_Click(object sender, RoutedEventArgs e)
+        {
+
+            int numer = int.Parse(coNty.Text);
+
+
+            DataClass[] tabela = dataClass.ToArray();
+            List<DataClass> dataClassWynik = new List<DataClass>();
+
+
+            for (int d = 0; d < tabela.Length; d++)
+            {
+                if (tabela[d].IDelem % numer != 0)
+                {
+                    dataClassWynik.Add(dataClass[d]);
+                }
+            }
+            dataClass.Clear();
+            dataClass = dataClassWynik;
+
+            Tabela.ItemsSource = null;
+            Tabela.ItemsSource = dataClass;
+            Tabela.DataContext = dataClass;
+        }
+
+        private void sortuj_Click(object sender, RoutedEventArgs e)
+        {
+            if (dataClass.Count() == 0)
+                return;
+
+            var timer = System.Diagnostics.Stopwatch.StartNew();
+
+            Sortowanie sortowanie = new Sortowanie(dataClass);
+
+            sortowanie.sortowanieNrSeriiSelectionSort();
+
+            dataClass = sortowanie.zwrot().ToList();
+
+            timer.Stop();
+
+            MessageBox.Show("czas milisekundach = " + timer.ElapsedMilliseconds + "\nIlość ticków -> " + timer.ElapsedTicks
+                + "\nCzas w nanosekundach -> " + timer.ElapsedTicks * 1000000000 / System.Diagnostics.Stopwatch.Frequency);
+         
+
+            Tabela.ItemsSource = null;
+            Tabela.ItemsSource = dataClass;
+            Tabela.DataContext = dataClass;
+        }
+
+        private void binarne_Click(object sender, RoutedEventArgs e) //działa
+        {
+
+            if (!(szukana.Text.Length < 0) || !int.TryParse(szukana.Text, out int n))
+                return;
+
+            Wyszukiwanie wyszukiwanie = new Wyszukiwanie(dataClass);
+            List<DataClass> wynik = new List<DataClass>();
+
+            var timer3 = System.Diagnostics.Stopwatch.StartNew();
+            wynik.Add(wyszukiwanie.binarne(n));
+            timer3.Stop();
+
+            MessageBox.Show("Czas szukania binarnego to mili -> " + timer3.ElapsedMilliseconds + "\nIlość ticków -> " + timer3.ElapsedTicks
+                + "\nCzas w nanosekundach -> " + timer3.ElapsedTicks * 1000000000 / System.Diagnostics.Stopwatch.Frequency 
+                );         
+
+            Tabela.ItemsSource = null;
+            Tabela.ItemsSource = wynik;
+            Tabela.DataContext = wynik;
+        }
+
+        private void liniowe_Click(object sender, RoutedEventArgs e)
+        {
+            if (!(szukana.Text.Length < 0) || !int.TryParse(szukana.Text, out int n))
+                return;
+
+            List<DataClass> wynik = new List<DataClass>();
+            Wyszukiwanie wyszukiwanie = new Wyszukiwanie(dataClass);
+
+            var timer = System.Diagnostics.Stopwatch.StartNew();
+            wynik = (wyszukiwanie.liniowe(n));
+            timer.Stop();
+
+            MessageBox.Show("Czas szukania binarnego to mili -> " + timer.ElapsedMilliseconds + "\nIlość ticków -> " + timer.ElapsedTicks
+              + "\nCzas w nanosekundach -> " + timer.ElapsedTicks * 1000000000 / System.Diagnostics.Stopwatch.Frequency
+              );
+
+
+            Tabela.ItemsSource = null;
+            Tabela.ItemsSource = wynik;
+            Tabela.DataContext = wynik;
         }
     }
 }
